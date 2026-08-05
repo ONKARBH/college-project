aa95cdbc539c1c0cb7d1e8d3420d44c1a9f37a04
Onkar Bhosale <your_email@gmail.com>
2026-08-05 21:48:37 +0530

    image format changed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 
-import slide1 from "../assets/images/slideImg/slide1.jpg";
-import slide2 from "../assets/images/slideImg/slide2.jpg";
-import slide3 from "../assets/images/slideImg/slide3.jpg";
-import slide4 from "../assets/images/slideImg/slide4.jpg";
-import slide5 from "../assets/images/slideImg/slide5.jpg";
-import slide6 from "../assets/images/slideImg/slide6.jpg";
-import slide7 from "../assets/images/slideImg/slide7.jpg";
+import slide1 from "../assets/images/slideImg/slide1.jpeg";
+import slide2 from "../assets/images/slideImg/slide2.jpeg";
+import slide3 from "../assets/images/slideImg/slide3.jpeg";
+import slide4 from "../assets/images/slideImg/slide4.jpeg";
+import slide5 from "../assets/images/slideImg/slide5.jpeg";
+import slide6 from "../assets/images/slideImg/slide6.jpeg";
+import slide7 from "../assets/images/slideImg/slide7.jpeg";
 
 import "../styles/slider.scss";
 
